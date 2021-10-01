2ac0f5affd5af7fadd7d135c189f81dd45fa536c
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-10-01 11:51:58 -0700

    Update logout.js

--- dashboard/logout.js
+++ dashboard/logout.js
@@ -3,13 +3,13 @@ function logout(r = null) {
         
         chrome.storage.local.clear(() => {
             console.log('Logged Out!')
-            document.location.href = document.location.origin + `/schoolloop/redesign/login/login.html?out=true&r=${encodeURI(JSON.stringify(r))}`
+            document.location.href = document.location.origin + `/looped/login/?out=true&r=${encodeURI(JSON.stringify(r))}`
         })
         
     } catch {
         
         setTimeout(() => {
-            document.location.href = document.location.origin + `hmbhs-web-extension/schoolloop/redesign/login/login.html?out=true&r=${encodeURI(JSON.stringify(r))}`
+            document.location.href = document.location.origin + `/looped/login/?out=true&r=${encodeURI(JSON.stringify(r))}`
         }, 3000)
         
     }
