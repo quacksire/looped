7cd9344f36efcf8678281918b64222e85a974630
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-10-23 14:32:47 -0700

    fixed

--- dashboard/class.js
+++ dashboard/class.js
@@ -22,25 +22,14 @@
     document.getElementById('className').innerHTML = `${courseInfo.course.name}`
     document.getElementById('teacherName').innerHTML = `${courseInfo.teacher.name}`
     console.info(`Loaded ${courseInfo.course.name} page (${id})`)
-<<<<<<< HEAD
         //try {
         //------------------------ Grade List ------------------------//
     courseInfo.grades.forEach(grade => {
-=======
-
-    try {
-
-        courseInfo.grades.forEach(grade => {
->>>>>>> parent of 631b40c (charts are cool)
             try {
                 document.getElementById('noItems').remove()
             } catch (e) {
                 //nada
             }
-<<<<<<< HEAD
-=======
-            //Want to do more here, like a pi chart or something
->>>>>>> parent of 631b40c (charts are cool)
             let listItem = document.createElement('li')
             listItem.className = 'list-group-item d-flex justify-content-between align-items-start'
             listItem.innerHTML = `
@@ -51,7 +40,6 @@
                     <span class="badge bg-primary rounded-pill">${String(grade.percentScore).split('.')[0]}%</span>`
             document.getElementById('assignmentList').appendChild(listItem)
         })
-<<<<<<< HEAD
         //------------------------ Chart ------------------------//
         /*
          *   Adapted from https://cdn.schoolloop.com/release_1.0.39.2/static//lib/chartjs/chart_class.js
@@ -117,117 +105,3 @@
         //    console.warn(`${courseInfo.course.name} has no grades`)
         //}
 })()
-=======
-
-        let data = courseInfo.trendScores
-
-        //let chart = new slChart({
-        //    canvasId: 'myChart',
-        //    id: user.students[0].studentID,
-        //    period_id: id,
-        //    isLongBeachScaledScore: false,
-        //    progress_report_data: courseInfo
-        //})
-
-
-
-
-        feather.replace({ 'aria-hidden': 'true' })
-
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-
-
-
-
-
-
-
-
-    } catch {
-        console.warn(`${courseInfo.course.name} has no grades`)
-    }
-
-
-})()
-/*
-var yLabel = this.isLongBeachScaledScore ? "Score" : "Percent";
-        var config = {
-            type: 'line',
-            data: {
-                labels: dates, // Hours
-
-                datasets: [{
-                    label: "Grade Trend",
-                    data: percentages,
-                    fill: false
-                }]
-            },
-
-            options: {
-                bezierCurve : false,
-                responsive: true,
-                scales: {
-                    xAxes: [
-                        {
-                            type: "time",
-                            display: true,
-                            time: {
-                                format: 'MM/DD/YY',
-                                displayFormat: 'MM/DD/YY'
-                            },
-                            ticks: {
-                                userCallback: function (dataLabel, index, data) {
-                                    return index % (data.length - 1) === 0 ? dataLabel : ''; //only first and last dates
-                                }
-                            }
-                        }
-                    ],
-                    yAxes: [
-                        {
-                            display: true,
-                            scaleLabel: {
-                                show: true,
-                                labelString: yLabel
-                            },
-                            ticks: {
-                                userCallback: function (dataLabel, index, data) {
-                                    return parseFloat(dataLabel.toFixed(2));
-                                }
-                            }
-                        }
-                    ]
-                }
-            }
-        };
-        var myLineChart = new Chart(this.context,config);
-    }
-    */
-/*
-ID: "1629184296239"
-​​​
-dayID: "1629442800000"
-​​​
-dropped: "false"
-​​​
-grade: "A+"
-​​​
-markIDString: "current"
-​​​
-numberOfZeros: "0"
-​​​
-periodID: "1593846839236"
-​​​
-score: "1.0"
-​​​
-standardsBased: "false"
-​​​
-studentID: "1593846838236"
-​​​
-teacherID: "1102472042704"
-​​​
-tourseID: "1376458792934"
-*/
->>>>>>> parent of 631b40c (charts are cool)
--- dashboard/logout.js
+++ dashboard/logout.js
@@ -9,17 +9,17 @@ function logout(r = null) {
     }
     setTimeout(() => {
         if (urlParams.has('page') && r) {
-            if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}` //Fixes Cache
-            document.location.href = `/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}`
+            //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}` //Fixes Cache
+            document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}`
         } else if (r) {
-            if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}` //Fixes Cache
-            document.location.href = `/login/?out=true&r=${encodeURI(String(r))}`
+            //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}` //Fixes Cache
+            document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}`
         } else if (urlParams.has('page')) {
-            if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&p=${urlParams.get('page')}` //Fixes Cache
-            document.location.href = `/login/?out=true&p=${urlParams.get('page')}`
+            //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&p=${urlParams.get('page')}` //Fixes Cache
+            document.location.href = `/looped/login/?out=true&p=${urlParams.get('page')}`
         } else {
-            if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true` //Fixes Cache
-            document.location.href = `/login/?out=true`
+            //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true` //Fixes Cache
+            document.location.href = `/looped/login/?out=true`
         }
     }, 500)
 }
