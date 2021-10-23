631b40ceee76a6dc79c1545e78722679253be8bd
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-10-22 19:06:10 -0700

    charts are cool

--- dashboard/class.js
+++ dashboard/class.js
@@ -2,6 +2,7 @@
     const QueryString = window.location.search;
     const urlParams = new URLSearchParams(QueryString);
     let user = JSON.parse(decodeURI(Cookies.get('slUser')))
+    let sl = console
     let auth = {
         headers: {
             'Authorization': `${user.auth}`,
@@ -25,28 +26,28 @@
     document.getElementById('teacherName').innerHTML = `${courseInfo.teacher.name}`
     console.info(`Loaded ${courseInfo.course.name} page (${id})`)
 
-    try {
+    //try {
 
-        courseInfo.grades.forEach(grade => {
-            try {
-                document.getElementById('noItems').remove()
-            } catch (e) {
-                //nada
-            }
-            //Want to do more here, like a pi chart or something
-            let listItem = document.createElement('li')
-            listItem.className = 'list-group-item d-flex justify-content-between align-items-start'
-            listItem.innerHTML = `
+    courseInfo.grades.forEach(grade => {
+        try {
+            document.getElementById('noItems').remove()
+        } catch (e) {
+            //nada
+        }
+        //Want to do more here, like a pi chart or something
+        let listItem = document.createElement('li')
+        listItem.className = 'list-group-item d-flex justify-content-between align-items-start'
+        listItem.innerHTML = `
                     <div class="ms-2 me-auto">
                     <div class="fw-bold">${grade.assignment.categoryName}</div>
                     ${grade.assignment.title}
                     </div>
                     <span class="badge bg-primary rounded-pill">${String(grade.percentScore).split('.')[0]}%</span>`
-            document.getElementById('assignmentList').appendChild(listItem)
-        })
+        document.getElementById('assignmentList').appendChild(listItem)
+    })
 
-        let data = courseInfo.trendScores
-
+    let data = courseInfo.trendScores
+    console.log(data)
         //let chart = new slChart({
         //    canvasId: 'myChart',
         //    id: user.students[0].studentID,
@@ -55,26 +56,97 @@
         //    progress_report_data: courseInfo
         //})
 
-
-
-
-        feather.replace({ 'aria-hidden': 'true' })
-
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-
-
-
-
-
-
-
-
-    } catch {
-        console.warn(`${courseInfo.course.name} has no grades`)
+    //console.log("data to process", data);
+    var percentages = [];
+    var dates = [];
+    for (var i = 0; i < courseInfo.trendScores.length; i++) {
+        percentages.push(parseFloat(courseInfo.trendScores[i].score) * 100);
+        dates.push(new Date(parseInt(String(courseInfo.trendScores[i].dayID))).toLocaleDateString())
     }
+    //sl.log("percentages", percentages)
+    //sl.log("dates", dates)
+
+    //sl.log("paint", percentages, dates)
+    var yLabel = "Score";
+    var config = {
+        type: 'line',
+        data: {
+            labels: dates, // Hours
+
+            datasets: [{
+                label: "Grade Trend",
+                data: percentages,
+                fill: false,
+                borderColor: 'rgba(248, 148, 6, 1)'
+            }]
+        },
+
+        options: {
+            bezierCurve: true,
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    display: true,
+                    ticks: {
+                        userCallback: function(dataLabel, index, data) {
+                            return index % (courseInfo.trendScores.length - 1) === 0 ? dataLabel : ''; //only first and last dates
+                        }
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        show: true,
+                        labelString: yLabel
+                    },
+                    ticks: {
+                        userCallback: function(dataLabel, index, data) {
+                            return parseFloat(dataLabel.toFixed(2));
+                        }
+                    }
+                }]
+            }
+        }
+    };
+    var myLineChart = new Chart('myChart', config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    feather.replace({ 'aria-hidden': 'true' })
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+
+
+
+
+
+
+
+    //} catch {
+    //    console.warn(`${courseInfo.course.name} has no grades`)
+    //}
 
 
 })()
