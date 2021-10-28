7ab81f881737d02c29ea373b430740a4e4af1dad
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-10-28 10:33:56 -0700

    Update loginRedirect.js

--- loginRedirect.js
+++ loginRedirect.js
@@ -4,13 +4,13 @@
         console.info('School Loop User Cookie Found! Sending to Dashboard')
         setTimeout(() => {
             //if (document.location.href.includes('/looped/')) document.location.href = `/looped/dashboard/`
-            document.location.replace(document.location.origin + `/looped/dashboard/`)
+            document.location.replace(document.location.origin + `/dashboard/`)
         }, 1500)
     } else {
         console.info('We found someone new! Sending to login page')
         setTimeout(() => {
             //if (document.location.href.includes('/looped/')) document.location.href = `/looped/login/`
-            document.location.replace(document.location.origin + `/looped/login/`)
+            document.location.replace(document.location.origin + `/login/`)
         }, 1500)
     }
 })()
