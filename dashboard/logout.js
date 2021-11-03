ee840d50e4159ee45c2c8fd274bd8375af2229da
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-11-02 20:45:42 -0700

    polish

--- dashboard/logout.js
+++ dashboard/logout.js
@@ -10,16 +10,16 @@ function logout(r = null) {
     setTimeout(() => {
         if (urlParams.has('page') && r) {
             //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}` //Fixes Cache
-            document.location.href = `/login/?out=true&r=${encodeURI(String(r))}&p=${urlParams.get('page')}`
+            document.location.href = `/login/?out&r=${encodeURI(String(r))}&p=${urlParams.get('page')}`
         } else if (r) {
             //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&r=${encodeURI(String(r))}` //Fixes Cache
-            document.location.href = `/login/?out=true&r=${encodeURI(String(r))}`
+            document.location.href = `/login/?out&r=${encodeURI(String(r))}`
         } else if (urlParams.has('page')) {
             //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true&p=${urlParams.get('page')}` //Fixes Cache
-            document.location.href = `/login/?out=true&p=${urlParams.get('page')}`
+            document.location.href = `/login/?out&p=${urlParams.get('page')}`
         } else {
             //if (document.location.href.includes('/looped/') || document.location.href.includes('/looped')) document.location.href = `/looped/login/?out=true` //Fixes Cache
-            document.location.href = `/login/?out=true`
+            document.location.href = `/login/?out`
         }
     }, 500)
 }
--- login/login.js
+++ login/login.js
@@ -1,16 +1,11 @@
 const QueryString = window.location.search;
 const urlParams = new URLSearchParams(QueryString);
 
-
-
 function validate(e) {
     e.preventDefault();
-
     const username = document.getElementById("floatingUsername");
     const password = document.getElementById("floatingPassword");
     console.log(`${username} : ${password}`)
-
-
     return valid;
 }
 
@@ -39,23 +34,10 @@ function getCookie(cname) {
 
 function login() {
     if (urlParams.has('p')) {
-        //if (document.location.href.includes('/looped/')) document.location.replace(document.location.origin + `/looped/dashboard/?page=${urlParams.get('p')}`)
         document.location.replace(document.location.origin + `/dashboard/?page=${urlParams.get('p')}`)
     } else {
-        //if (document.location.href.includes('/looped/')) document.location.replace(document.location.origin + `/looped/dashboard/}`)
         document.location.replace(document.location.origin + `/dashboard/`)
     }
-
-
-
-}
-
-function error() {
-
-    //document.location.href = document.location.origin + `/looped/login/?failed=true`
-    document.location.replace(document.location.origin + `/login/?failed=true`)
-
-
 }
 
 function alert(message, type) {
@@ -66,45 +48,10 @@ function alert(message, type) {
 }
 
 function incorrectRole(role) {
-
     document.getElementById('badRole').innerHTML = String(role)
-
     var myModal = new bootstrap.Modal(document.getElementById('roleError'), {})
     myModal.toggle()
-
 }
-
-
-//document.getElementById("submit").addEventListener("click", validate);
-
-//chrome.storage.local.clear()
-
-let welcome = `
- __          __  _                          _ \n
- \ \        / / | |                        | |\n
-  \ \  /\  / /__| | ___ ___  _ __ ___   ___| |\n
-   \ \/  \/ / _ \ |/ __/ _ \| '_ \` _ \ / _ \ |\n
-    \  /\  /  __/ | (_| (_) | | | | | |  __/_|\n
-     \/  \/ \___|_|\___\___/|_| |_| |_|\___(_)\n
-`
-
-
-//Cookie checker
-/*
-try {
-    if (browser.storage.local.get("user")) login()
-} catch {
-    try {
-        if (chrome.storage.local.get("user")) login()
-    } catch {
-        try {
-            if (getCookie('user')) login()
-        } catch {
-            console.warn('No User Detected')
-        }
-    }
-}
-*/
 async function checkUser(user, pass) {
     console.log(`Trying to login with key: ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}`)
             let response = await fetch(`https://hmbhs.schoolloop.com/mapi/login?version=3&devToken=${encodeURI('Looped')}&devOS=${encodeURI(navigator.userAgent)}&year=${new Date().getFullYear()}`, {
@@ -114,30 +61,20 @@ async function checkUser(user, pass) {
             })
             console.log(response)
              //document.getElementById('runas').innerHTML = response
-    
-    
-    
         try {
             console.log(response)
             if (response.statusText == 'OK' || response.statusText == '') {
                 response = await response.json()
                 //response.role = 'admin'
                 document.getElementById('runas').innerHTML = response
-                if (response.role == 'student') {
-                    response.auth = `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}` //Save Auth header for future use
+                if (response.role == 'student') { //Student Support only (for now)
+                    response.auth = `Basic ${btoa(`${encodeURI(user)}:${encodeURI(pass)}`)}` //Save Auth header in auth cookie cause there's iframes involved
                     //response.role = 'admin'
-                    
                     setCookie('slUser', encodeURI(JSON.stringify(response)), 7)
                     alert(`<div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Please Wait, Signing you in</span></div> Signing you in...`, 'success')
                     setTimeout(() => {
-
                         login()
-                    
                     }, 1000)//2500
-                    
-                    
-                    
-                    
                     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 } else {
                     incorrectRole(response.role)
@@ -151,47 +88,20 @@ async function checkUser(user, pass) {
             alert(`Error ${e}`, 'danger')
             return
         }
-        
-        
-    
-             
         //login()
-
-
 }
 
 
-
-
-// vvv OLD vvv
-async function main() {
-    
-    if (urlParams.get('r') === 'null' && String(window.location.href).includes('?')) {
-        console.log('Logged Out')
-        document.getElementById('error').outerHTML = `<div class="toast position-absoulute bottom-0 end-0 text-white bg-primary border-0 position-absolute" role="alert" aria-live="assertive" aria-atomic="true" id='loggedOutToast'>
-            <div class="d-flex">
-                <div class="toast-body">
-                    You have been logged out.
-                </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>`
-            //loggedOutToast
-    } else {
-
-        console.log('[LEGACY] No URI Params')
-
-    }
-}
-
+//Entire Script
 document.getElementById('runas').innerHTML = `Web`
-
 if (urlParams.has('r')) incorrectRole(urlParams.get('r'))
-main()
-
+if (urlParams.has('out')) {
+    var toast = new bootstrap.Toast(document.getElementById('loggedOutToast'))
+    toast.show()
+}
 if (/\bCrOS\b/.test(navigator.userAgent)) alert('ChromeOS support is not great, things might break', 'warning')
 
-
+//JQuery Stuff
 $(document).ready(function() {
     $('.toast').toast('show');
     setTimeout(() => {
@@ -201,14 +111,5 @@ $(document).ready(function() {
 $('#login').bind('submit', function(e) {
     e.preventDefault();
     console.log('Welcome!');
-    console.info('hi')
     checkUser($('#floatingUsername').val(), $('#floatingPassword').val());
 });
-/*
-$('#login').bind('click', function(e) {
-    e.preventDefault();
-    console.log('Welcome!');
-    document.getElementById('runas').innerHTML = `Hi`
-    checkUser($('#floatingUsername').val(), $('#floatingPassword').val());
-});
-*/
