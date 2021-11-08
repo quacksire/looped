02845288e085ec05ed03b6e7932947d5c55f0a29
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-11-08 09:58:58 -0800

    Update pwaCheck.js

--- dashboard/pwaCheck.js
+++ dashboard/pwaCheck.js
@@ -35,10 +35,10 @@ function mobileCollapse(p = null) {
     }
 
 }
-let loadedSize = window.innerWidth
-document.addEventListener("resize", () => {
-    if (loadedSize >= 800 && loadedSize >= window.innerWidth || loadedSize <= 800 && loadedSize <= window.innerWidth) window.location.reload()
-})
-window.addEventListener("resize", () => {
-    if (loadedSize >= 800 && loadedSize >= window.innerWidth || loadedSize <= 800 && loadedSize <= window.innerWidth) window.location.reload()
-})
+//let loadedSize = window.innerWidth
+//document.addEventListener("resize", () => {
+//    if (loadedSize >= 800 && loadedSize >= window.innerWidth || loadedSize <= 800 && loadedSize <= window.innerWidth) window.location.reload()
+//})
+//window.addEventListener("resize", () => {
+//    if (loadedSize >= 800 && loadedSize >= window.innerWidth || loadedSize <= 800 && loadedSize <= window.innerWidth) window.location.reload()
+//})
