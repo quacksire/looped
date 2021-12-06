ec71608081c246d68b7b52ed9a5c093d04cb5c1e
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-12-06 12:16:06 -0800

    spin

--- loginRedirect.js
+++ loginRedirect.js
@@ -2,14 +2,11 @@
     if (!navigator.cookieEnabled) console.error('NO COOKIES!!')
     if (Cookies.get('slUser')) {
         console.info('School Loop User Cookie Found! Sending to Dashboard')
-        setTimeout(() => {
-            //if (document.location.href.includes('/looped/')) document.location.href = `/looped/dashboard/`
-            document.location.replace(document.location.origin + `/dashboard/`)
+        setTimeout(() => {            document.location.replace(document.location.origin + `/dashboard/`)
         }, 1500)
     } else {
         console.info('We found someone new! Sending to login page')
         setTimeout(() => {
-            //if (document.location.href.includes('/looped/')) document.location.href = `/looped/login/`
             document.location.replace(document.location.origin + `/login/`)
         }, 1500)
     }
