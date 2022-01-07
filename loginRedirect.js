55fc5106534ce838da5e3cdff131ad68aed3d8a4
Quacksire <19170969+quacksire@users.noreply.github.com>
2022-01-07 17:14:04 +0000

    small change

--- loginRedirect.js
+++ loginRedirect.js
@@ -1,12 +1,12 @@
 (async function() {
-    if (!navigator.cookieEnabled) console.error('NO COOKIES!!')
+    if (!navigator.cookieEnabled) // TODO: DO something if cookies are disabled
     if (Cookies.get('slUser')) {
-        console.info('School Loop User Cookie Found! Sending to Dashboard')
+        //School Loop User Cookie Found! Sending to Dashboard
         setTimeout(() => {
             document.location.replace(document.location.origin + `/dashboard/`)
         }, 1500)
     } else {
-        console.info('We found someone new! Sending to login page')
+        //We found someone new! Sending to login page
         setTimeout(() => {
             document.location.replace(document.location.origin + `/login/`)
         }, 1500)
