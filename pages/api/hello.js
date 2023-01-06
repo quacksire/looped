bc1e829095afdeb05fc1d0d92f7561c2864d45f5
child_duckling <sam@quacksire.dev>
2023-01-05 21:37:52 -0800

    edge runtime v2

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-
-
-import {getCookies} from "cookies-next";
-
-export default function handler(req, res) {
-  res.status(200).json(getCookies({req, res}))
-}
