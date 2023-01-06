d233d739c6b609ee71b05198a9290f5abd3f62a4
child_duckling <sam@quacksire.dev>
2023-01-06 14:38:37 -0800

    edge runtime v9

--- functions/_sl/news.js
+++ functions/_sl/news.js
@@ -1,14 +1,15 @@
 
 export async function onRequestGet(context) {
+    let cookies = context.request.headers.get('Cookie') || ""
+    let cooks = cookies.split(';')
+    let token = cooks[0].split('=')[1]
+    let uid =  cooks[1].split('=')[1]
     try {
         let cookies = context.request.headers.get('Cookie') || ""
         if (!cookies.includes("sl-token") || !cookies.includes("sl-token")) {
             return new Response('Not logged in', {status: 401})
         }
 
-        let cooks = cookies.split(';')
-        let token = cooks[0].split('=')[1]
-        let uid =  cooks[1].split('=')[1]
 
         //"https://\(domainName)/mapi/report_card?studentID=\(studentID)"
 
@@ -33,6 +34,6 @@ export async function onRequestGet(context) {
             }
         )
     } catch (e) {
-        return new Response(e, {status: 500})
+        return new Response(`${e} | token: ${token} | uid: ${uid}`, {status: 500})
     }
 }
