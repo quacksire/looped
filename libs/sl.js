32587d64919a89a12cd36a673025cbd7ad369ddc
child_duckling <sam@quacksire.dev>
2023-01-06 17:35:29 -0800

    axios might make it

--- libs/sl.js
+++ libs/sl.js
@@ -1,8 +1,10 @@
 import {getCookie} from "cookies-next";
+import axios from "axios";
+export const fetcher = (url) =>
+    axios
+        .get(url, { headers: {
+                'X-SL-User': `${getCookie('sl-token')}:${getCookie('sl-uid')}`
+            }})
+        .then((res) => res.data);
 
 
-export const fetcher = (url) => fetch(url, {
-    headers: {
-        'X-SL-User': `${getCookie('sl-token')}:${getCookie('sl-uid')}`
-    },
-    }).then((res) => res.json());
